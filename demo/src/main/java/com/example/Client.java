099ca52ce49621b7ad7713cf11a00668371859ef
Davide Susini <susini.davide@itismeucci.com>
2024-10-28 13:16:40 +0100

    commit

--- demo/src/main/java/com/example/Client.java
+++ demo/src/main/java/com/example/Client.java
@@ -11,18 +11,31 @@ import java.util.Scanner;
 public class Client {
     public static void main(String[] args) throws UnknownHostException, IOException {
         System.out.println("Hello world!");
-        Socket mySocket = new Socket("127.0.0.1", 3645);
+        Socket mySocket = new Socket("127.0.0.1", 3000);
+        
 
         BufferedReader in = new BufferedReader(new InputStreamReader(mySocket.getInputStream()));
         DataOutputStream out = new DataOutputStream(mySocket.getOutputStream());
         String outputString;
         Scanner sc = new Scanner(System.in);
+        String stringInput = "";
         do {
-            System.out.println("Inserisci la stringa: ");
+            System.out.print("#: ");
             outputString = sc.nextLine();
             out.writeBytes(outputString + "\n");
-            String stringInput = in.readLine();
-            System.out.println("stringa ricevuta: " + stringInput);
+            if (outputString.equals("?")) {
+                while (true) {
+                    stringInput = in.readLine();
+                    if (stringInput.equals("@")) {
+                        break;
+                    } else {
+                        System.out.println(stringInput);
+                    }
+                }
+            } else {
+                stringInput = in.readLine();
+                System.out.println(stringInput);
+            }
         } while (!outputString.equals("!"));
         System.out.println("Chiusura della connessione a seguito di invio di ! o di un'eccezzione");
         sc.close();
